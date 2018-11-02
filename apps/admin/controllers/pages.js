3dc96a734d04183068bed88ee967dd8d9f75d065
nttlong <zugeliang2000@gmail.com>
2018-11-02 18:04:07 +0700

    save

--- apps/admin/controllers/pages.js
+++ apps/admin/controllers/pages.js
@@ -1,3 +1,4 @@
 module.exports=(context,req,res)=>{
+    var x=context.getCurrentUrl();
     context.htmlPagePath="pages/"+req.params.page+".html";
 }
---
+++ apps/admin/controllers/users.js
@@ -0,0 +1,3 @@
+module.exports=(context,req,res)=>{
+    debugger;
+}
