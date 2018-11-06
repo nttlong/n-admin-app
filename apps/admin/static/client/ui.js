69f9748b32dc0e39e436a2ecc72bf36ed3da062e
nttlong <zugeliang2000@gmail.com>
2018-11-06 11:40:10 +0700

    save

--- apps/admin/static/client/ui.js
+++ apps/admin/static/client/ui.js
@@ -1,4 +1,5 @@
-﻿var _dialog_root_url;
+﻿!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?n():"function"==typeof define&&define.amd?define(n):n()}(0,function(){"use strict";function e(e){var n=this.constructor;return this.then(function(t){return n.resolve(e()).then(function(){return t})},function(t){return n.resolve(e()).then(function(){return n.reject(t)})})}function n(){}function t(e){if(!(this instanceof t))throw new TypeError("Promises must be constructed via new");if("function"!=typeof e)throw new TypeError("not a function");this._state=0,this._handled=!1,this._value=undefined,this._deferreds=[],u(e,this)}function o(e,n){for(;3===e._state;)e=e._value;0!==e._state?(e._handled=!0,t._immediateFn(function(){var t=1===e._state?n.onFulfilled:n.onRejected;if(null!==t){var o;try{o=t(e._value)}catch(f){return void i(n.promise,f)}r(n.promise,o)}else(1===e._state?r:i)(n.promise,e._value)})):e._deferreds.push(n)}function r(e,n){try{if(n===e)throw new TypeError("A promise cannot be resolved with itself.");if(n&&("object"==typeof n||"function"==typeof n)){var o=n.then;if(n instanceof t)return e._state=3,e._value=n,void f(e);if("function"==typeof o)return void u(function(e,n){return function(){e.apply(n,arguments)}}(o,n),e)}e._state=1,e._value=n,f(e)}catch(r){i(e,r)}}function i(e,n){e._state=2,e._value=n,f(e)}function f(e){2===e._state&&0===e._deferreds.length&&t._immediateFn(function(){e._handled||t._unhandledRejectionFn(e._value)});for(var n=0,r=e._deferreds.length;r>n;n++)o(e,e._deferreds[n]);e._deferreds=null}function u(e,n){var t=!1;try{e(function(e){t||(t=!0,r(n,e))},function(e){t||(t=!0,i(n,e))})}catch(o){if(t)return;t=!0,i(n,o)}}var c=setTimeout;t.prototype["catch"]=function(e){return this.then(null,e)},t.prototype.then=function(e,t){var r=new this.constructor(n);return o(this,new function(e,n,t){this.onFulfilled="function"==typeof e?e:null,this.onRejected="function"==typeof n?n:null,this.promise=t}(e,t,r)),r},t.prototype["finally"]=e,t.all=function(e){return new t(function(n,t){function o(e,f){try{if(f&&("object"==typeof f||"function"==typeof f)){var u=f.then;if("function"==typeof u)return void u.call(f,function(n){o(e,n)},t)}r[e]=f,0==--i&&n(r)}catch(c){t(c)}}if(!e||"undefined"==typeof e.length)throw new TypeError("Promise.all accepts an array");var r=Array.prototype.slice.call(e);if(0===r.length)return n([]);for(var i=r.length,f=0;r.length>f;f++)o(f,r[f])})},t.resolve=function(e){return e&&"object"==typeof e&&e.constructor===t?e:new t(function(n){n(e)})},t.reject=function(e){return new t(function(n,t){t(e)})},t.race=function(e){return new t(function(n,t){for(var o=0,r=e.length;r>o;o++)e[o].then(n,t)})},t._immediateFn="function"==typeof setImmediate&&function(e){setImmediate(e)}||function(e){c(e,0)},t._unhandledRejectionFn=function(e){void 0!==console&&console&&console.warn("Possible Unhandled Promise Rejection:",e)};var l=function(){if("undefined"!=typeof self)return self;if("undefined"!=typeof window)return window;if("undefined"!=typeof global)return global;throw Error("unable to locate global object")}();"Promise"in l?l.Promise.prototype["finally"]||(l.Promise.prototype["finally"]=e):l.Promise=t});
+var _dialog_root_url;
 function dialog_root_url(value) {
     _dialog_root_url = value;
 }
@@ -599,3 +600,206 @@ angular.module('imageupload', [])
             }
         };
     });
+mdl.service("$ajax",[function(){
+    var instance={
+        onBeforeCall:undefined,
+        onAfterCall:undefined
+    }
+    function executor(owner,id){
+        this.owner=owner;
+        this._id=id;
+    }
+    executor.prototype.__exec=function(callback,_id,_data){
+        var me=this;
+        var sender=undefined;
+        if(instance.onBeforeCall){
+            sender=instance.onBeforeCall();
+        }
+        var callId=_id;
+        var callData=_data;
+        if(!_id){
+            callId=me._id;
+        }
+        if(!_data){
+            callData=me._data;
+        }
+        $.ajax({
+            url:me.owner.url,
+            method:"POST",
+            headers:{"AJAX-POST":callId},
+            data:JSON.stringify(callData),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success:function(res){
+                if(instance.onAfterCall){
+                    instance.onAfterCall(me,sender);
+                }
+                if(callback){
+                    callback(undefined,res);
+                }
+            },
+            error:function(ex){
+                if(instance.onAfterCall){
+                    instance.onAfterCall(me,sender);
+                }
+                if(callback){
+                    callback(ex,undefined);
+                }
+            }
+
+        });
+    }
+    executor.prototype.with=function(data){
+        this._data=data;
+        return this;
+        
+    }
+    executor.prototype.set=function(data){
+        var keys=Object.keys(data);
+        if(!this._data){
+            this._data=data;
+        }
+        for(var i=0;i<keys.length;i++){
+            var key=keys[i];
+            var val=data[key];
+            this._data[key]=val;
+        }
+        return this;
+    }
+    executor.prototype.done=function(callback){
+        var me=this;
+        if(!callback){
+            return new Promise(function(resolve,reject){
+                me.__exec(function(e,r){
+                    if(e){
+                        reject(e);
+                    }
+                    else {
+                        resolve(r);
+                    }
+                });
+            });
+        } else {
+            return me.__exec(callback);
+        }
+    }
+    function Caller(url){
+        this.url=url;
+    }
+   
+    
+    Caller.prototype.call=function(id,data,callback){
+        var ret= new executor(this,id);
+        if(typeof data=="function"){
+            callback=data;
+            data=undefined;
+        }
+        if(data){
+            ret.with(data);
+        }
+        if(callback){
+            return ret.__exec(callback);
+        }
+        return ret;
+    }
+    Caller.prototype.callAll=function(){
+        var promises=[];
+        var _executor=new executor(this);
+        var args=arguments;
+        for(var i=0;i<args.length;i++){
+            promises.push(new Promise(function(resole,reject){
+                var arg=args[i];
+                _executor.__exec(function(e,r){
+                    if(e){
+                        reject(e);
+                    }
+                    else {
+                        if(arg.done){
+                            arg.done(r);
+                            resole(r);
+                        }
+                    }
+                    
+                },arg.id,arg.data);
+            }));
+        }
+        return Promise.all(promises);
+    }
+    return {
+        setOnBeforeCall:function(cb){
+            instance.onAfterCall=cb;
+        },
+        setOnAfterCall:function(cb){
+            instance.onAfterCall=cb;
+        },
+        with:function(url){
+            return new Caller(url);
+        }
+
+    }
+}])
+mdl.directive("ajax",["$ajax","$parse",function($ajax,$parse){
+    return {
+        restrict:"ECA",
+        transclude:true,
+        replace:true,
+        priority:1,
+        scope:false,
+        template:"<ajax-caller ng-transclude></ajax-caller>",
+        link:function(s,e,a){
+            e.attr("s-id",angular.element(e[0]).scope().$id);
+            if(a.ws){
+                $parse(a.ws).assign(s,$ajax.with(a.url));
+            }
+            else {
+                $parse("$ws").assign(s,$ajax.with(a.url));
+            }
+        }
+    }
+}]);
+/**
+ * <call data-id="ajax function name" [data-params="param variable"] [data-callback="callback function name"] [data-function="function name"/>
+ */
+mdl.directive("call",["$parse",function($parse){
+    return {
+        restrict:"ECA",
+        priority:50000,
+        scope:false,
+        link:function(s,e,a){
+            s.$watch(e.parent().attr("ws")||"$ws",function(o,v){
+                var ws=undefined;
+                function exec(){
+                    var scope=findScopeById(e.parent().attr("s-id")*1);
+                        var data=scope.$eval(a.params);
+                        ws.call(a.id,data,function(e,r){
+                            if(e){
+                                throw(e);
+                                return;
+                            }
+                            if(a.callback){
+                                var fn=scope.$eval(a.callback);
+                                if(angular.isFunction(fn)){
+                                    fn(r);
+                                }
+                            }
+                            if(a.ngModel){
+                                $parse(a.ngModel).assign(scope,r);
+                                scope.$applyAsync();
+                            }
+                        });
+                }
+                if(angular.isDefined(v)){
+                    ws=v;
+                    if(a.id && (!a.function)){
+                        exec();
+
+                    }
+                    else {
+                        $parse(a.function).assign(exec);
+                    }
+                }
+            })
+            
+        }
+    }
+}])
