e83c46b0cd8950539b578b102cd054c2609e3c44
nttlong <zugeliang2000@gamil.com>
2018-11-12 19:43:23 +0700

    save

--- pkgs/business-info/model.js
+++ pkgs/business-info/model.js
@@ -1,53 +1,47 @@
 var mUtils=require("n-qr");
+var BSONTypes=require("n-qr").BSONTypes;
 var modelName = "sys.business_info"
 mUtils.model(modelName,[
     
 ],{
     properties:{
         site:{
-            bsonType:mUtils.BSONTypes.Object,
-            
+            bsonType:BSONTypes.Object,
+            required: ["title", "key_words", "name", "mobile", "address", "email"],
+            properties:{
+                title: { bsonType: BSONTypes.String},
+                key_words: { bsonType: BSONTypes.String},
+                name:{bsonType:BSONTypes.String},
+                address:{bsonType:BSONTypes.String},
+                email:{bsonType:BSONTypes.String},
+                description:{bsonType:BSONTypes.String}
+            }
+        },
+        settings:{
+            bsonType:BSONTypes.Object,
+            email:{
+                bsonType:BSONTypes.Object,
+                required: [
+                    "server",
+                    "username",
+                    "password",
+                    "port",
+                    "use_default_credentials",
+                    "is_use_ssl",
+                    "email"
+                ],
+                properties: {
+                    email: { bsonType: BSONTypes.String },
+                    username: { bsonType: BSONTypes.String },
+                    password: { bsonType: BSONTypes.String },
+                    port: { bsonType: BSONTypes.Int32 },
+                    is_use_ssl: { bsonType: BSONTypes.Boolean },
+                    email: { bsonType: BSONTypes.String }
+                }
+
+            }
+           
         }
     }
-   
-    site:mUtils.embeded(mUtils.FieldTypes.Object,[
-        "title", "key_words", "name", "mobile", "address","email"
-    ],{
-            name: mUtils.embeded(mUtils.FieldTypes.Object, [
-
-            ], {
-                    en: mUtils.FieldTypes.String,
-                    vn: mUtils.FieldTypes.String
-                }),
-            mobile: mUtils.FieldTypes.String,
-            address: mUtils.FieldTypes.String,
-            email: mUtils.FieldTypes.String,
-        title:mUtils.FieldTypes.Object,
-        description:mUtils.FieldTypes.String,
-        key_words:mUtils.FieldTypes.String
-
-    }),
-    settings:mUtils.embeded(mUtils.FieldTypes.String,[
-        "server",
-        "username",
-        "password",
-        "port",
-        "use_default_credentials",
-        "is_use_ssl",
-        "email"
-
-
-    ],{
-        server:mUtils.FieldTypes.String,
-        username:mUtils.FieldTypes.String,
-        password:mUtils.FieldTypes.String,
-        port:mUtils.FieldTypes.Int32,
-        is_use_ssl :mUtils.FieldTypes.Boolean,
-        use_default_credentials :mUtils.FieldTypes.Boolean,
-        email:mUtils.FieldTypes.String
-
-    })
-    
-
-})
+});
 module.exports=modelName;
